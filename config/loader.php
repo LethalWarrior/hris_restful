13dcdc9b9b1193835a548b6f82ff7ee9551e6e13
LethalWarrior <albertfiraz@gmail.com>
2024-05-02 03:53:34 +0700

    feat: load HttpExceptions in loader

--- config/loader.php
+++ config/loader.php
@@ -7,6 +7,7 @@ $loader = new Loader();
 $loader->registerNamespaces([
     'App\Models' => realpath(__DIR__ . '/../app/models/'),
     'App\Controllers' => realpath(__DIR__ . '/../app/controllers/'),
+    'App\Controllers\Exceptions' => realpath(__DIR__ . '/../app/controllers/exceptions/'),
     'App\Services' => realpath(__DIR__ . '/../app/services/'),
     'App\Middlewares' => realpath(__DIR__ . '/../app/middlewares/')
 ]);
