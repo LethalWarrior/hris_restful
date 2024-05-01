9196f05191e19c4279d9fec124a912056581148c
LethalWarrior <albertfiraz@gmail.com>
2024-05-01 18:27:28 +0700

    feat: create basic error handling on /api/users

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use Exception;
+use Phalcon\Http\Response;
 use Phalcon\Mvc\Controller;
 
 class UsersController extends Controller
@@ -13,26 +15,33 @@ class UsersController extends Controller
 
     public function getUsers()
     {
-        if ($this->modelsManager === null) {
-            throw new \Exception('modelsManager is null');
+        try {
+            if ($this->modelsManager === null) {
+                throw new \Exception('modelsManager is null');
+            }
+
+            $query = 'SELECT * FROM App\Models\Users ORDER BY id';
+
+            $users = $this->modelsManager->executeQuery($query);
+
+            $data = [];
+
+            foreach ($users as $user) {
+                $data[] = [
+                    'id' => $user->id,
+                    'fullname' => $user->fullname,
+                    'username' => $user->username,
+                    'created_at' => $user->created_at,
+                    'updated_at' => $user->updated_at,
+                ];
+            }
+
+            echo json_encode($data);
+        } catch (Exception $e) {
+            $response = new Response();
+            $response->setStatusCode(500);
+            $response->setJsonContent(["error" => "It seems that there is an error in the server"]);
+            $response->send();
         }
-
-        $query = 'SELECT * FROM App\Models\Users ORDER BY id';
-
-        $users = $this->modelsManager->executeQuery($query);
-
-        $data = [];
-
-        foreach ($users as $user) {
-            $data[] = [
-                'id' => $user->id,
-                'fullname' => $user->fullname,
-                'username' => $user->username,
-                'created_at' => $user->created_at,
-                'updated_at' => $user->updated_at,
-            ];
-        }
-
-        echo json_encode($data);
     }
 }
